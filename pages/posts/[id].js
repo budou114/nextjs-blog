6890b88a3eff80867a05a16713bbc61fa268cbfc
budou114 <58349030+budou114@users.noreply.github.com>
2022-07-12 08:40:55 +0900

    MarkDownをレンダリングしよう

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -9,13 +9,14 @@ export default function Post({ postData }) {
       {postData.id}
       <br />
       {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   );
 }
 
 export async function getStaticPaths() {
-  const paths = getAllPostIds();
-  console.log(paths);
+  const paths = await getAllPostIds();
   return {
     paths,
     fallback: false,
@@ -23,7 +24,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
+  const postData = await getPostData(params.id);
   return {
     props: {
       postData,
